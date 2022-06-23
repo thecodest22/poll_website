8b767244d752e35072a627b3dc1bc4bd6657665a
codest <thecodest@ya.ru>
2022-06-24 01:26:36 +0300

    superuser created, models added

--- poll_website/polls/models.py
+++ poll_website/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -9,9 +12,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)  # Задаем максимальную длину цифро-буквенного поля
     pub_date = models.DateTimeField('date published')  # Поле для даты и времени, необязательный арг - имя поля
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  # Это поле отношений, т.е. связи между моделями.
                                                                       # ПОКА ХЗ ЧТО ЭТО
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)  # Числовое поле с нулем по дефолту
+
+    def __str__(self):
+        return self.choice_text
