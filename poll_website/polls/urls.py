36de98bbaf74c7f92dca8f3a4bcd3bfe7b56d4f5
codest <thecodest@ya.ru>
2022-06-25 00:53:38 +0300

    added some urls and views for polls app

--- poll_website/polls/urls.py
+++ poll_website/polls/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', include([
+        path('', views.detail, name='detail'),
+        path('results/', views.results, name='results'),
+        path('vote/', views.vote, name='vote'),
+    ])),
 ]
