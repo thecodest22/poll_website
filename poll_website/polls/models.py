e30d8e6bb2f8dbfa9acc836c06ed79f1d53f28e5
codest <thecodest@ya.ru>
2022-06-29 01:15:06 +0300

    app template added, models and views updated

--- poll_website/polls/models.py
+++ poll_website/polls/models.py
@@ -11,11 +11,11 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)  # Задаем максимальную длину цифро-буквенного поля
     pub_date = models.DateTimeField('date published')  # Поле для даты и времени, необязательный арг - имя поля
-
+    # objects = models.Manager()
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
+    def was_published_recently(self) -> bool:
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
